3236a21de70a657ff3fe301b51c7ac6fa8fffeed
lsingh3 <lokenathsingh@yahoo.com>
2017-11-10 19:14:34 +0530

    Quick sort

--- algo.js
+++ algo.js
@@ -33,6 +33,50 @@ function mergeSort(arr,l,r){
 		merge(arr,l,m,r);
 	}
 }
+
+//Quick Sort
+//***************************************************************************************************************************
+function qsort(arr,low,high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[pi] is 
+          now at right place */
+        var pi = partition(arr, low, high);
+
+        // Recursively sort elements before
+        // partition and after partition
+        qsort(arr, low, pi-1);
+        qsort(arr, pi+1, high);
+    }
+}
+
+function partition(arr, low, high)
+{
+    var pivot = arr[high]; 
+    var i = (low-1); // index of smaller element
+    for (var j=low; j<high; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;
+
+            // swap arr[i] and arr[j]
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // swap arr[i+1] and arr[high] (or pivot)
+    var temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+
+    return i+1;
+}
 //String reverse
 //***************************************************************************************************************************
 String.prototype.reverese = function(){
