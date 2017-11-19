6efd64d4583794a06b19bb91a7c75fbb305d6bac
lsingh3 <lokenathsingh@yahoo.com>
2017-11-19 17:18:49 +0530

    Linked list reverse fix

--- linkedlist.js
+++ linkedlist.js
@@ -87,16 +87,16 @@ SinglyList.prototype.remove = function(position) {
     return deletedListNode;
 };
 
-Node reverse(Node node) {
-    Node prev = null;
-    Node current = node;
-    Node next = null;
+SinglyList.prototype.reverse = function() {
+    var prev = null;
+    var current = this.head;
+    var next = null;
     while (current != null) {
         next = current.next;
         current.next = prev;
         prev = current;
         current = next;
     }
-    node = prev;
-    return node;
+    this.head = prev;
+    return list;
 }
